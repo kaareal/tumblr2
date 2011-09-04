951bf82cdee7a2b2775caff16d256e9949a5dae9
Kaare <kaare.a.larsen@gmail.com>
2011-09-04 20:28:35 +0300

    Remove codes

--- test/tumblr-conf.js
+++ test/tumblr-conf.js
@@ -1,7 +1,6 @@
-
 module.exports = {
-  consumerKey :  'WWDbUJghkvYSEls68tC8ZwkqAvI40tawdyK4HwkbMNVd6XOIag', //'your key provided by tumblr',
-  consumerSecret : 'c9ROEHfEKGyEUSbXxWXKS5djSkrqE1GIDmMTiathifIe65rey2', //you secret provide by tumblr,
-  accessTokenKey:  'i0Cgmz6zDpQxyT3ZcLJPM9XUMv0tmlw0GyDwzO97GcYZoKeVW0' ,//'STATE YOUR NAME',
-	accessTokenSecret: 'KW4n5uVEBVH43WKXbtroJVMHt3QpBcBtfXj9sk1RCxTQFXrydk'//'STATE YOUR NAME'
+  consumerKey :  'your key provided by tumblr'', ,
+  consumerSecret : 'you secret provide by tumblr', 
+  accessTokenKey:  'access token',
+  accessTokenSecret: 'access token secret'
 }
